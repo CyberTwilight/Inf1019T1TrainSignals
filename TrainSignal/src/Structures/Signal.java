5ece1dc8ac10ccd6d1ac801eeeb0682afffbb404
Gabriel Medeiros <gmgabmedeiros@hotmail.com>
2017-10-17 05:30:39 -0200

    Add files via upload
    Projeto adicionado ao github
    
    Últimas mudanças feitas por Gabriel:
    
    Método GetCurrentState, que retorna uma string com o nome simples do estado atual, implementado no SystemController
    Switch / Case implementado no Panel, que troca a imagem do semáforo (de verde para vermelho ou vice versa) dependendo do estado.

---
+++ TrainSignal/src/Structures/Signal.java
@@ -0,0 +1,15 @@
+package Structures;
+
+public abstract class Signal {
+	private static SystemController sc;
+	public static Signal getInitialState (SystemController syscont) {
+		sc = syscont;
+		return new BothWaysOpen();
+	}
+	public abstract Signal LeftEntranceSensorActivated();
+	public abstract Signal LeftExitSensorActivated();
+	public abstract  Signal RightEntranceSensorActivated();
+	public abstract Signal RightExitSensorActivated();
+	public abstract String CurrentState();
+	
+}
